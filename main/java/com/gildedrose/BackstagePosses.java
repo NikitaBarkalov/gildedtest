f2ce9a8ef3e4684db14ec6c417eff0e202e4d93f
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-01 16:46:19 +0200

    *Added classes which extend Item

---
+++ main/java/com/gildedrose/BackstagePosses.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class BackstagePosses extends Item{
+    public BackstagePosses(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
